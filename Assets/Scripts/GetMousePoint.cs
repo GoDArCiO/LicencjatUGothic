e4544e691f60f1814c5f6f7cf1dc971281fc7a9a
GoDArCiO <46372307+GoDArCiO@users.noreply.github.com>
2021-02-26 23:55:42 +0100

    Interactable

--- Assets/Scripts/GetMousePoint.cs
+++ Assets/Scripts/GetMousePoint.cs
@@ -5,6 +5,7 @@ using UnityEngine.AI;
 
 public class GetMousePoint : MonoBehaviour
 {
+    public Character character;
     public Camera cam;
     public NavMeshAgent navAgent;
 
@@ -12,16 +13,40 @@ public class GetMousePoint : MonoBehaviour
 
     public Animator animator;
 
+    private Interactable currentInteractable;
+    public float interactDistance;
+
     void Update()
     {
         Plane plane = new Plane(Vector3.up, 0);
 
         float distance;
+        RaycastHit hit;
         Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
         if (plane.Raycast(ray, out distance))
         {
             worldPosition = ray.GetPoint(distance);
-            if (Input.GetMouseButtonDown(0)) navAgent.SetDestination(worldPosition);
+            if (Input.GetMouseButtonDown(0))
+            {
+                currentInteractable = null;
+                navAgent.SetDestination(worldPosition);
+
+                if (Physics.Raycast(ray, out hit))
+                {
+                    currentInteractable = GameObjectTools.SearchForScript<Interactable>(hit.transform.gameObject, true, true);
+
+                    if (currentInteractable != null)
+                    {
+                        navAgent.SetDestination(currentInteractable.transform.position);
+                    }
+                    
+                }
+            }
+        }
+
+        if(currentInteractable != null && Vector3.Distance(transform.position,currentInteractable.transform.position)<interactDistance)
+        {
+            currentInteractable.Interact(character);
         }
 
         animator.SetFloat("Blend", navAgent.velocity.magnitude / navAgent.speed);
