19a335b8d51d0334b5032a1c82058a2f551d7f92
GoDArCiO <46372307+GoDArCiO@users.noreply.github.com>
2021-02-26 14:24:47 +0100

    Utility Scripts

---
+++ Assets/Scripts/Utils/GameObjectTools.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameObjectTools : MonoBehaviour
+{
+    public static T SearchForScript<T>(GameObject gameObject, bool searchInParent = false, bool searchInChildren = false) where T : MonoBehaviour
+    {
+        T script = null;
+
+        script = gameObject.GetComponent<T>();
+
+        if (!script && searchInParent)
+        {
+            script = gameObject.GetComponentInParent<T>();
+        }
+        if (!script && searchInChildren)
+        {
+            script = gameObject.GetComponentInChildren<T>();
+        }
+
+        return script;
+    }
+
+}
