f222f9754e4d94b4d87b99b89a881f30cd0d2fbe
GoDArCiO <46372307+GoDArCiO@users.noreply.github.com>
2021-02-26 14:42:51 +0100

    Navigation

---
+++ Assets/Scripts/GetMousePoint.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class GetMousePoint : MonoBehaviour
+{
+    public Camera cam;
+    public NavMeshAgent navAgent;
+
+    public Vector3 worldPosition;
+
+    void Update()
+    {
+        Plane plane = new Plane(Vector3.up, 0);
+
+        float distance;
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        if (plane.Raycast(ray, out distance))
+        {
+            worldPosition = ray.GetPoint(distance);
+            if (Input.GetMouseButtonDown(0)) navAgent.SetDestination(worldPosition);
+        }
+    }
+}
